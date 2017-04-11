578c7bb4253db4418470eb4d39598974dc49d57b
Julian Jamardo <jamardo.julian@gmail.com>
2017-04-11 11:05:03 -0300

    Adding CppTestClient and CppTestServer

---
+++ CppTcpClient/CppTcpClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CppTcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string addr = "127.0.0.1";
+            WinsockConnect(addr, addr.Length);
+            //unsafe
+            //{
+            //    fixed (char* p = addr)
+            //    {
+            //        Console.WriteLine("The address stored in p: {0:X2}", (int)p);
+            //        WinsockConnect(p);
+            //    }
+            //}
+        }
+
+        [DllImport("WinsockTcpClient.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void WinsockConnect(string addr, int size);
+    }
+}
---
+++ CppTcpServer/CppTcpServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CppTcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WinsockAccept();
+        }
+
+        [DllImport("WinsockTcpServer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void WinsockAccept();
+    }
+}
